7f2332be391a72a333c2e0bbf688d1aebd41d5c9
William Broxton <williambroxton@Williams-iMac.local>
2021-12-02 11:19:07 -0800

    component 1 render

---
+++ src/pages/IntakeFormPage/IntakeFormPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./IntakeFormPage.css"
+
+export default function IntakeFormPage() {
+  return (
+    <>
+    <h1>Intake Form</h1>
+    <form className="dDivs">
+      <div className="qboxes">Type of Project</div>
+      <div className="qboxes">Starting State</div>
+      <div className="qboxes">3</div>
+
+      <div className="qboxes">4</div>
+      <div className="qboxes"></div>
+      <div className="qboxes">5</div>
+      
+      <div className="qboxes">6</div>
+      <div className="qboxes">7</div>
+      <div className="qboxes">8</div>
+    </form>
+    </>
+  );
+}
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./LandingPage.css"
+
+export default function NewOrderPage() {
+  return (
+    <>
+    <h1>NewOrderPage</h1>
+    <main className="dDivs">
+      <div className="qboxes">Type of Project</div>
+      <div className="qboxes">Starting State</div>
+      <div className="qboxes">3</div>
+
+      <div className="qboxes">4</div>
+      <div className="qboxes"></div>
+      <div className="qboxes">5</div>
+      
+      <div className="qboxes">6</div>
+      <div className="qboxes">7</div>
+      <div className="qboxes">8</div>
+    </main>
+    </>
+  );
+}
